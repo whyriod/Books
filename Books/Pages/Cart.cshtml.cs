41e21065ba6cd060ca4ac487e72d55386b84d525
whyriod <whyriod@byu.edu>
2022-02-25 12:31:44 -0700

    Mission Complete :)

--- Books/Pages/Cart.cshtml.cs
+++ Books/Pages/Cart.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Books.Infastructure;
 using Books.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -18,18 +19,23 @@ namespace Books.Pages
         }
 
         public Basket Basket { get; set; }
+        public string ReturnUrl { get; set; } //Make it persist.
 
-        public void OnGet(Basket b)
+        public void OnGet(string returnUrl)
         {
-            Basket = b;
+            ReturnUrl = returnUrl ?? "/";
+            Basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
         }
-        public IActionResult OnPost(int bookId)
+        public IActionResult OnPost(int bookId, string returnUrl)
         {
+            //?? Null Coalesing Operator. If the first isnt null use that, else use the thing on the right.
             var book = repo.BookRepo.FirstOrDefault(x => x.BookId == bookId);
-            Basket = new Basket();
+            Basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
             Basket.AddItem(book, 1);
 
-            return RedirectToPage();
+            HttpContext.Session.SetJson("basket", Basket);
+
+            return RedirectToPage(new { ReturnUrl = returnUrl });
         }
     }
 }
