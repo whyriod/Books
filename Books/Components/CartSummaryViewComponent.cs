fddace664d64aa2b15b40262034ae95a9b1aef2c
whyriod <whyriod@byu.edu>
2022-03-07 16:12:43 -0700

    Added the cart icon

---
+++ Books/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Books.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Basket cart;
+        public CartSummaryViewComponent(Basket cartService)
+        {
+            cart = cartService;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
